557fb5a5a3c2384e3476ead82ff478cf1dbb1af6
GOTO-TSL <kosuke.iosappbuilding@gmail.com>
2021-06-15 01:18:04 +0900

    Initial Commit

---
+++ HeroDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeroDemo
+//
+//  Created by 後藤孝輔 on 2021/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
